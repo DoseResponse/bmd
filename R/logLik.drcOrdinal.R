320e5614cc18a4e8e0db17a16a47e35b49c3c70b
JensBaalkilde <jba@plen.ku.dk>
2023-11-14 16:03:02 +0100

    Change "categories" to "levels" in several places.

--- R/logLik.drcOrdinal.R
+++ R/logLik.drcOrdinal.R
@@ -1,10 +1,10 @@
 logLik.drcOrdinal <- function(object, epsilon = 10^(-16)){
-  tmp <- sapply(1:length(object$categories), function(cat.i){
-    cat <- object$categories[[cat.i]]
+  tmp <- sapply(1:length(object$levels), function(cat.i){
+    cat <- object$levels[[cat.i]]
     cat.per.dose <- unlist(object$drmList[[1]]$origData[,cat])
     if(cat.i == 1){
       p.fun <- function(x) 1 - object$drmList[[cat.i]]$curve[[1]](x)
-    } else if(cat.i == length(object$categories)){
+    } else if(cat.i == length(object$levels)){
       p.fun <- function(x) object$drmList[[cat.i-1]]$curve[[1]](x)
     } else{
       p.fun <- function(x) object$drmList[[cat.i-1]]$curve[[1]](x) - object$drmList[[cat.i]]$curve[[1]](x)
--- R/plot.drcOrdinal.R
+++ R/plot.drcOrdinal.R
@@ -1,20 +1,20 @@
 plot.drcOrdinal <- function(object, col_pal = NULL, xlim = NULL){
   if(is.null(col_pal)){
-    col_pal <- scales::grey_pal(start = 0.9, end = 0)(length(object$categories))
+    col_pal <- scales::grey_pal(start = 0.9, end = 0)(length(object$levels))
   }
   
-  plotData <- pivot_longer(object$data, cols = object$categories) %>% #-c(object$dose, object$weights)) %>% 
+  plotData <- pivot_longer(object$data, cols = object$levels) %>% #-c(object$dose, object$weights)) %>% 
     mutate(dose = eval(parse(text=object$dose)),
-           cat = factor(name, levels = object$categories),
+           cat = factor(name, levels = object$levels),
            prop = value/eval(parse(text=object$weights)))
   
   plot <- ggplot(plotData) +
     geom_col(aes(x = dose, y = prop, fill = cat), alpha = 0.5) +
-    scale_fill_manual(breaks = object$categories, values = col_pal[1:length(object$categories)]) +
-    lapply(object$catMerged, 
-           function(catMerged){ 
-             geom_function(aes(col = catMerged), fun = object$drmList[[catMerged]]$curve[[1]])}) +
-    scale_color_manual(breaks = object$catMerged, values = col_pal[2:length(object$categories)]) +
+    scale_fill_manual(breaks = object$levels, values = col_pal[1:length(object$levels)]) +
+    lapply(object$levelsMerged, 
+           function(levelsMerged){ 
+             geom_function(aes(col = levelsMerged), fun = object$drmList[[levelsMerged]]$curve[[1]])}) +
+    scale_color_manual(breaks = object$levelsMerged, values = col_pal[2:length(object$levels)]) +
     scale_x_continuous(trans = scales:::pseudo_log_trans(sigma = 1, base = exp(1)), limits = xlim) +
     scale_y_continuous(limits = c(0,1)) +
     labs(x = object$dose, y = "proportion") +
