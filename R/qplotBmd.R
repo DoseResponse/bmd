970c06d8c94f0f468e44a4b88223b30a9661f845
JensBaalkilde <jba@plen.ku.dk>
2023-12-01 13:29:21 +0100

    ggplot2 and dplyr dependencies.

--- R/qplotBmd.R
+++ R/qplotBmd.R
@@ -12,18 +12,18 @@ qplotBmd <- function(x, ..., interval = c("BMDL", "twosided", "none"), add = FAL
   xLow <- rep(object$interval[1],2)
   xUpp <- rep(object$interval[2],2)
   
-  returnLayers <- list(ggplot2:::geom_line(aes(x = xHoriz, y = yHoriz), linetype = 2),
-                       ggplot2:::geom_line(aes(x = xVert, y = yVert)))
+  returnLayers <- list(geom_line(aes(x = xHoriz, y = yHoriz), linetype = 2),
+                       geom_line(aes(x = xVert, y = yVert)))
   
   if(interval == "BMDL"){
     returnLayers <- c(returnLayers,
-                      ggplot2:::geom_line(aes(x = xInt, y = yHoriz)),
-                      ggplot2:::geom_line(aes(x = xLow, y = yVert)))
+                      geom_line(aes(x = xInt, y = yHoriz)),
+                      geom_line(aes(x = xLow, y = yVert)))
   } else if(interval == "twosided"){
     returnLayers <- c(returnLayers,
-                      ggplot2:::geom_line(aes(x = xInt, y = yHoriz)),
-                      ggplot2:::geom_line(aes(x = xLow, y = yVert)),
-                      ggplot2:::geom_line(aes(x = xUpp, y = yVert)))
+                      geom_line(aes(x = xInt, y = yHoriz)),
+                      geom_line(aes(x = xLow, y = yVert)),
+                      geom_line(aes(x = xUpp, y = yVert)))
   }
   
   if(add){
