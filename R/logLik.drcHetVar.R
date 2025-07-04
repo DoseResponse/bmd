8a7de655c2a825951c10a946b3b96224b327a581
Zhenglei Gao <zhenglei.gao@baer.com>
2025-07-04 18:42:29 +0200

    solve the AIC.drcHetVar issue

--- R/logLik.drcHetVar.R
+++ R/logLik.drcHetVar.R
@@ -11,13 +11,28 @@ logLik.drcHetVar <- function(object, ...){
   - object$sumList$numObs/2 * log(2*pi) - object$value/2
 }
 
-#' @title S3 method
+#' AIC Method for drcHetVar Objects
+#'
+#' Extracts the AIC from drcHetVar model objects.
+#'
+#' @param object A drcHetVar model object
+#' @param ... Additional arguments (not used)
+#' @param k Numeric value for penalty term in AIC calculation (default is 2)
+#'
+#' @return The log-likelihood value
 #' @export
 AIC.drcHetVar <- function(object, ..., k = 2){
   2*object$sumList$numPar - k*logLik(object)
 }
 
-#' @title S3 method
+#' BIC Method for drcHetVar Objects
+#'
+#' Extracts the BIC from drcHetVar model objects.
+#'
+#' @param object A drcHetVar model object
+#' @param ... Additional arguments (not used)
+#'
+#' @return The log-likelihood value
 #' @export
 BIC.drcHetVar <- function(object, ...){
   object$sumList$numPar*log(object$sumList$numObs) - 2*logLik(object)
